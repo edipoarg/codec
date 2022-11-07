bcfb2c2fd78f65a3f316c2b24c408455dd11ce38
Niv Sardi <xaiki@evilgiggle.com>
2022-11-07 09:47:10 -0300

    rewrite our custom store with get/set, because update(null) resets the store
    contrary to popular belief, returning null/undefined from update(s => {}) will
    just set that store to null, (i.e. you have no way to cancel the update), so we
    need to do a whole get/set dance to get the same result… thank you svelte.
    
    this was found by @blackm2021 that actually looked into the store code and
    found it to be quite buggy, more on this later.

--- src/stores/utils.js
+++ src/stores/utils.js
@@ -1,5 +1,5 @@
+import { writable, get } from "svelte/store";
 import EventEmitter from "events";
-import { writable } from "svelte/store";
 
 const object_equal = (a, b) => {
     if (!a || !b) return false;
@@ -20,17 +20,16 @@ export const makeStore = (t, idField = "UAR") => {
     const _s = writable(t);
     const { subscribe, set, update } = _s;
 
-    const push = (media, emit = true) => _s.update(prev => {
-        if (!prev) return { [media[idField]]: media };
-
-        if (object_equal(prev[media[idField]], media)) {
-            return;
-        }
+    const push = (media, emit = true) => {
+        const prev = get(_s);
+        if (!prev) return _s.set({ [media[idField]]: media });
+        if (object_equal(prev[media[idField]], media)) return;
 
         emit && _e.emit("push", media);
         prev[media[idField]] = media;
-        return prev;
-    });
+        return _s.set(prev)
+    };
+
     return {
         subscribe,
         set,
